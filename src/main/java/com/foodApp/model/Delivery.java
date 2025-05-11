6ddd5aebc7c88502d66b9d8938eded3ba98df716
AliJohari05 <alitehranijohari1384@gmail.com>
2025-05-11 19:44:37 +0330

    I am completing the folder class model.

---
+++ src/main/java/com/foodApp/model/Delivery.java
@@ -0,0 +1,4 @@
+package com.foodApp.model;
+
+public class Delivery {
+}
--- src/main/java/com/foodApp/model/Restaurant.java
+++ src/main/java/com/foodApp/model/Restaurant.java
@@ -1,4 +1,120 @@
 package com.foodApp.model;
-
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "restaurants")
 public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false , length = 100)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false,length = 20,unique = true)
+    private String phone;
+
+    @Column(name = "working_hours", nullable = false, length = 100)
+    private String workingHours;
+
+    @Column(name = "logo_url", nullable = false)
+    private String logoUrl;
+
+    private boolean approved;
+
+    @ManyToOne // Each user (seller) can have multiple restaurants.
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    // === Getters and Setters ===
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getWorkingHours() {
+        return workingHours;
+    }
+
+    public void setWorkingHours(String workingHours) {
+        this.workingHours = workingHours;
+    }
+
+    public String getLogoUrl() {
+        return logoUrl;
+    }
+
+    public void setLogoUrl(String logoUrl) {
+        this.logoUrl = logoUrl;
+    }
+
+    public boolean isApproved() {
+        return approved;
+    }
+
+    public void setApproved(boolean approved) {
+        this.approved = approved;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
