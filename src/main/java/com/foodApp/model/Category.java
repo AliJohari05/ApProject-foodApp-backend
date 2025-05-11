be27b88951380976e68857951cdce7726dd2b84b
AliJohari05 <alitehranijohari1384@gmail.com>
2025-05-11 20:07:15 +0330

    Category modified

--- src/main/java/com/foodApp/model/Category.java
+++ src/main/java/com/foodApp/model/Category.java
@@ -1,4 +1,78 @@
 package com.foodApp.model;
-
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false,length = 100)
+    private String title;
+
+    private String description;
+
+    @Column(name = "display_order")
+    private int displayOrder;
+
+    @Column(length = 255)
+    private String icon;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    // === Getters & Setters ===
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getDisplayOrder() {
+        return displayOrder;
+    }
+
+    public void setDisplayOrder(int displayOrder) {
+        this.displayOrder = displayOrder;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
