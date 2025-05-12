070b5511679dbf5a8484240c13e370e94a51a283
AliJohari05 <alitehranijohari1384@gmail.com>
2025-05-12 21:28:35 +0330

    The delivery class has been completed

--- src/main/java/com/foodApp/model/Delivery.java
+++ src/main/java/com/foodApp/model/Delivery.java
@@ -1,4 +1,91 @@
 package com.foodApp.model;
+import jakarta.persistence.*;
+import org.hibernate.annotations.JoinColumnOrFormula;
 
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "deliveries")
 public class Delivery {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToOne
+    @JoinColumn(name = "order_id",nullable = false)
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "delivery_person_id",nullable = false)
+    private User deliveryPerson;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(name = "assigned_at")
+    private LocalDateTime assignedAt;
+
+    @Column(name = "delivered_at")
+    private LocalDateTime deliveredAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    // === Getters & Setters ===
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public User getDeliveryPerson() {
+        return deliveryPerson;
+    }
+
+    public void setDeliveryPerson(User deliveryPerson) {
+        this.deliveryPerson = deliveryPerson;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getAssignedAt() {
+        return assignedAt;
+    }
+
+    public void setAssignedAt(LocalDateTime assignedAt) {
+        this.assignedAt = assignedAt;
+    }
+
+    public LocalDateTime getDeliveredAt() {
+        return deliveredAt;
+    }
+
+    public void setDeliveredAt(LocalDateTime deliveredAt) {
+        this.deliveredAt = deliveredAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
