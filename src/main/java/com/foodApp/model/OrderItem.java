12fc9a7ce28d2f8e505bc5f833fec3455748304f
AliJohari05 <alitehranijohari1384@gmail.com>
2025-05-12 21:10:59 +0330

    The body of the class is completed

--- src/main/java/com/foodApp/model/OrderItem.java
+++ src/main/java/com/foodApp/model/OrderItem.java
@@ -9,4 +9,64 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "order_items")
 public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id",nullable = false)
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "menu_item_id",nullable = false)
+    private MenuItem menuItem;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(name = "price_at_order", nullable = false)
+    private BigDecimal priceAtOrder;
+
+    // === Getters & Setters ===
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public MenuItem getMenuItem() {
+        return menuItem;
+    }
+
+    public void setMenuItem(MenuItem menuItem) {
+        this.menuItem = menuItem;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPriceAtOrder() {
+        return priceAtOrder;
+    }
+
+    public void setPriceAtOrder(BigDecimal priceAtOrder) {
+        this.priceAtOrder = priceAtOrder;
+    }
 }
