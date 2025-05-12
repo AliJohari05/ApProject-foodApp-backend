f941e4e2b7ea0a3277198bca186d23667b0ed790
AliJohari05 <alitehranijohari1384@gmail.com>
2025-05-12 21:10:32 +0330

    Required libraries added.

--- src/main/java/com/foodApp/model/OrderItem.java
+++ src/main/java/com/foodApp/model/OrderItem.java
@@ -1,4 +1,12 @@
 package com.foodApp.model;
 
+import java.awt.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "order_items")
 public class OrderItem {
 }
