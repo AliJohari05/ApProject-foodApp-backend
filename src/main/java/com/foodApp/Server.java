0b166a4627391f4221cd5154d1d12c02214bdb59
AliJohari05 <alitehranijohari1384@gmail.com>
2025-05-13 23:30:33 +0330

    Database connection completed.

--- src/main/java/com/foodApp/Server.java
+++ src/main/java/com/foodApp/Server.java
@@ -1,5 +1,7 @@
 package com.foodApp;
 
+import com.foodApp.model.Role;
+import com.foodApp.model.User;
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpExchange;//Representing a request and response
 import com.sun.net.httpserver.HttpHandler;//Interface for creating route handlers
@@ -8,23 +10,30 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;//Specifying the server address and port
 
+import com.foodApp.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class Server {
     public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/api/hello", new HelloHandler());
-        server.setExecutor(null);
-        server.start();
-        System.out.println("🚀 Server started on http://localhost:8080");
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+
+        User user = new User();
+        user.setName("Ali Johari");
+        user.setPhone("09121234988");
+        user.setEmail("ali@example.com");
+        user.setPassword("123456");
+        user.setRole(Role.CUSTOMER);
+        user.setAddress("Tehran-khiaban dolat-bolvar kave");
+
+        session.persist(user);
+        tx.commit();
+        session.close();
+
+        System.out.println("User saved successfully!");
     }
 
-    static class HelloHandler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange exchange) throws IOException {
-            String response = "hiiii";
-            exchange.sendResponseHeaders(200, response.getBytes().length);
-            OutputStream os = exchange.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        }
-    }
+
 }
--- src/main/java/com/foodApp/config/AppConfig.java
+++ src/main/java/com/foodApp/config/AppConfig.java
@@ -1,12 +1,6 @@
 package com.foodApp.config;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@EnableWebMvc
-@ComponentScan("com.foodApp")
-public class AppConfig implements WebMvcConfigurer {
+
+public class AppConfig {
 }
