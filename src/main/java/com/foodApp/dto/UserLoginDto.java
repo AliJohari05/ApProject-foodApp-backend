fdb29f98d7414f992a2ecd975d6b0646c0e87f71
AliJohari05 <alitehranijohari1384@gmail.com>
2025-05-16 19:16:00 +0330

    complete LoginDto

--- src/main/java/com/foodApp/dto/UserLoginDto.java
+++ src/main/java/com/foodApp/dto/UserLoginDto.java
@@ -1,4 +1,22 @@
 package com.foodApp.dto;
 
 public class UserLoginDto {
+    private String phone;
+    private String password;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
