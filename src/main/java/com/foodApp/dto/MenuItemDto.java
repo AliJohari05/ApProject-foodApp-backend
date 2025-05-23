827459dc7afe5d1fd8e0fdcbe39ccaea08ce1697
Amin <m.aminagahi@gmail.com>
2025-05-23 21:18:31 +0330

    Dto

---
+++ src/main/java/com/foodApp/dto/MenuItemDto.java
@@ -0,0 +1,62 @@
+package com.foodApp.dto;
+
+import java.util.List;
+
+public class MenuItemDto {
+private Integer id;
+private String name;
+private String imageBase64;
+private String description;
+private Integer price;
+private Integer supply;
+private List<String> categories;
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageBase64() {
+        return imageBase64;
+    }
+    public void setImageBase64(String imageBase64) {
+        this.imageBase64 = imageBase64;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getSupply() {
+        return supply;
+    }
+    public void setSupply(Integer supply) {
+        this.supply = supply;
+    }
+
+    public List<String> getCategories() {
+        return categories;
+    }
+    public void setCategories(List<String> categories) {
+        this.categories = categories;
+    }
+}
