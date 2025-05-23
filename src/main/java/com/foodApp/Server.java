3cb88cd7ca9032a9c8b09219e172ef78914be03e
AliJohari05 <alitehranijohari1384@gmail.com>
2025-05-23 17:51:12 +0330

    complete LogoutHandler

--- src/main/java/com/foodApp/Server.java
+++ src/main/java/com/foodApp/Server.java
@@ -5,6 +5,7 @@ import com.foodApp.httpHandler.restaurant.ApprovedRestaurantHandler;
 import com.foodApp.httpHandler.restaurant.GetRestaurantsByOwnerHandler;
 import com.foodApp.httpHandler.restaurant.RegisterRestaurantHandler;
 import com.foodApp.httpHandler.user.LoginHandler;
+import com.foodApp.httpHandler.user.LogoutHandler;
 import com.foodApp.httpHandler.user.ProfileHandler;
 import com.foodApp.httpHandler.user.SignUpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -20,6 +21,7 @@ public class Server {
             server.createContext("/auth/register", new SignUpHandler());
             server.createContext("/auth/login", new LoginHandler());
             server.createContext("/auth/profile", new ProfileHandler());
+            server.createContext("/auth/logout", new LogoutHandler());
             server.createContext("/Food4U/restaurant", new RegisterRestaurantHandler());
             server.createContext("/Food4U/restaurant/approved", new ApprovedRestaurantHandler());
             server.createContext("/Food4U/restaurant/owner", new GetRestaurantsByOwnerHandler());
