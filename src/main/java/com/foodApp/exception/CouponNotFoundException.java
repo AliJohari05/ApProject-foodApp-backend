ea497d270d287da6e0d5c710c936fb90f8e522ab
AliJohari05 <alitehranijohari1384@gmail.com>
2025-06-24 13:41:41 +0330

    create CouponNotFoundException

---
+++ src/main/java/com/foodApp/exception/CouponNotFoundException.java
@@ -0,0 +1,7 @@
+package com.foodApp.exception;
+
+public class CouponNotFoundException extends RuntimeException {
+    public CouponNotFoundException(String message) {
+        super(message);
+    }
+}
